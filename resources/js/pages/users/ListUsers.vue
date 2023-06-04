ad2816077626d26a08bf4ba560f7391afe263ebc
jowel007 <abuhanifjowel1@gmail.com>
2023-06-04 23:40:27 +0600

    laravel vue multi-purpose application

---
+++ resources/js/pages/users/ListUsers.vue
@@ -0,0 +1,195 @@
+
+
+<template>
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">User List</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">user</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="content">
+            <div class="container-fluid">
+
+                <button type="button" class="mb-2 btn btn-primary" data-toggle="modal" data-target="#createUserModal">
+                    Add New User
+                </button>
+
+                <div class="card">
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead>
+                            <tr>
+                                <th style="width: 10px">#</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Registered</th>
+                                <th>Role</th>
+                                <th>Option</th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            <tr v-for="(user, index ) in users" :key="user.id">
+                                <td>{{ index +1 }}</td>
+                                <td>{{ user.name }}</td>
+                                <td>{{ user.email }}</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    <div class="modal fade" id="createUserModal" data-backdrop="static" tabindex="-1" role="dialog"
+         aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Add New User</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <Form @submit="createUser" :validation-schema="schema" v-slot="{ errors }">
+                <div class="modal-body">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <Field name="name"  type="text" class="form-control" :class="{'is-invalid':errors.name}" aria-describedby="nameHelp"
+                                   placeholder="Enter full name" />
+                                <span class="invalid-feedback">{{ errors.name }}</span>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <Field name="email" type="email" class="form-control" :class="{'is-invalid':errors.email}" aria-describedby="nameHelp"
+                                   placeholder="Enter full name" />
+                            <span class="invalid-feedback">{{ errors.email }}</span>
+                        </div>
+                   
+
+                    <div class="form-group">
+                        <label for="email">Password</label>
+                        <Field name="password"  type="password" class="form-control" :class="{'is-invalid':errors.password}" aria-describedby="nameHelp"
+                               placeholder="Enter password" />
+                        <span class="invalid-feedback">{{ errors.password }}</span>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button  type="submit" class="btn btn-primary">Save</button>
+                </div>
+            </Form>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="createUserModal" data-backdrop="static" tabindex="-1" role="dialog"
+                   aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Add New User</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input  type="text" class="form-control "  aria-describedby="nameHelp"
+                               placeholder="Enter full name">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input  type="email" class="form-control "  aria-describedby="nameHelp"
+                               placeholder="Enter full name">
+                    </div>
+               
+
+                <div class="form-group">
+                    <label for="email">Password</label>
+                    <input  type="password" class="form-control"  aria-describedby="nameHelp"
+                           placeholder="Enter password">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <button  type="button" class="btn btn-primary">Save</button>
+            </div>
+        </form>
+        </div>
+    </div>
+</div>
+
+</template>
+
+
+<script setup>
+
+import axios from 'axios';
+import {ref,onMounted, reactive} from "vue";
+import { Form, Field } from 'vee-validate';
+import * as yup from 'yup';
+    const users = ref([]);
+
+    const getUsers = () => {
+        axios.get('/api/users')
+            .then((response) =>{
+                users.value = response.data;
+            })
+
+    }
+
+    //client side validation
+    const schema = yup.object({
+        name:yup.string().required(),
+        email:yup.string().email().required(),
+        password:yup.string().required().min(8),
+    });
+
+
+    const createUser = (values,{ resetForm }) => {
+        axios.post('/api/users',values)
+        .then((response) =>{
+             users.value.unshift(response.data);
+            $('#createUserModal').modal('hide');
+            resetForm();
+        });
+
+    }
+        //click event
+    // const createUser = () =>{
+    //     axios.post('/api/users',form)
+    //     .then((response) =>{
+    //         users.value.unshift(response.data);
+    //         form.name = '';
+    //         form.email = '';
+    //         form.password = '';
+
+    //         $('#createUserModal').modal('hide');
+    //     });
+    // }
+
+    onMounted(()=>{
+        getUsers();
+    });
+
+</script>
